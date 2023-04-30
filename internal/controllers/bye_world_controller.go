a75fa04362ee222aa9f111fc03192969009ba5d7
Sudeep Patel <sudeeppatel@Sudeeps-MacBook-Air.local>
2023-04-30 12:47:11 +0530

    updated local.yml

--- internal/controllers/bye_world_controller.go
+++ internal/controllers/bye_world_controller.go
@@ -6,5 +6,5 @@ import (
 )
 
 func ByeWorldController(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Bye World! Yes it is indeed working")
+	fmt.Fprintf(w, "Bye World! yes this is working")
 }
